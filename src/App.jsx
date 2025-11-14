954e6601eb72f730cb7569130ebf95cb0e4419a3
Shanu Ojha <146368454+Shanu-Ojha@users.noreply.github.com>
2025-11-14 06:25:39 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Search from './pages/Search'
+
+const App = () => {
+  return (
+    <Router>
+      <div className="min-h-screen bg-black text-white">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
